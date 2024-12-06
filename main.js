8873421d7b2be6102f1b87d204d6cbd15558e0a9
DostonbekZaripov <dastandev0110@gmail.com>
2024-12-07 02:56:48 +0500

    the end

---
+++ main.js
@@ -0,0 +1,41 @@
+// let input1 = [1,2,2,3]
+// let input2 = [3,2,1,2]
+// let res1= input1.sort()
+// let res2= input2.sort()
+// function Arrays(res1, res2){
+//     for(let i = 0; i<res1.length; i++){
+//         if(res1[i]!==res2[i]){
+//             return false
+//         }
+//     }
+//     return true
+// }
+// console.log(Arrays(res1,res2))
+
+
+// let arr = [4,2,9,7,5]
+// let n = arr.sort()
+// function Arrayss(n){
+//     for(let i = 0; i<n.length; i++){
+//         n.length[i].splice()
+//     }
+//     return 
+// }
+
+// console.log(Arrayss(3))
+
+// let input = [3, 3, 1, 3, 1];
+
+// function ikkixil(input) {
+//     let birxil = []; 
+//     for (let i = 0; i < input.length; i++) {
+//       if (!birxil.includes(input[i])) {
+//         birxil.push(input[i]);
+//       }
+//       if (birxil.length > 2) {
+//         return false;
+//     }
+//     }
+//     return birxil.length === 2;
+//   }
+// console.log(ikkixil(input));
